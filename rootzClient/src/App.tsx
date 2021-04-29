dc49e173ae59a04c7ded5b4794238d482b6d2a1e
patriceowensdesigns <patriceowensdesigns@gmail.com>
2021-04-29 18:56:46 -0400

    app.tsx code changes

--- rootzClient/src/App.tsx
+++ rootzClient/src/App.tsx
@@ -8,7 +8,7 @@ import Main from './components/Main';
 function App() {
   const [token, setToken] = useState('');
   const [role, setRole] = useState('');
-
+  
   return (
     <div className="App">
       <Main setToken={setToken} setRole={setRole} />
